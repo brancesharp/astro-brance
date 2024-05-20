bfea8cd1aa2bd7052e137fe0fd0b7a0d91d906fe
brancesharp <bpsharp01@gmail.com>
2024-05-19 21:22:53 -0700

    added blog post layout

--- src/content/config.js
+++ src/content/config.js
@@ -2,10 +2,13 @@
 import { z, defineCollection } from 'astro:content';
 
 // 2. Define a `type` and `schema` for each collection
-const blogCollection = defineCollection({
+const postsCollection = defineCollection({
   type: 'content', // v2.5.0 and later
   schema: z.object({
     title: z.string(),
+    pubDate: z.date(),
+    description: z.string(),
+    author: z.string(),
     tags: z.array(z.string()),
     image: z.string().optional(),
   }),
@@ -13,5 +16,5 @@ const blogCollection = defineCollection({
 
 // 3. Export a single `collections` object to register your collection(s)
 export const collections = {
-  'blog': blogCollection,
+  'posts': postsCollection,
 };
---
+++ src/content/posts/first-post.md
@@ -0,0 +1,9 @@
+---
+title: 'My First Blog Post'
+pubDate: 2024-05-19
+description: 'Building an Astro website'
+author: 'Brance Sharp'
+tags: ["astro", "blogging"]
+---
+
+### The first blog post
